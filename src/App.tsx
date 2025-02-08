a35852a8ac4fa1023821889380d634a890ea02df
212601662 <shreshth.kandari@wabtec.com>
2025-02-08 00:34:43 -0600

    feat: Implement movie fetching with loading and error states

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,31 @@ const API_OPTIONS = {
 const App = () => {
   const [searchTitle, setSearchTitle] = useState('')
   const [error, setError] = useState('')
+  const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   const fetchMovies = async () => {
+    setIsLoading(true)
+    setError('')
     try {
       const endpoint = `${BASE_URL}/discover/movie?sort_by=popularity.desc`
       const response = await fetch(endpoint, API_OPTIONS)
       const data = await response.json()
-      console.log(data)
+
+      if (data.results.length === 0) {
+        setError('No movies found')
+        setMovies([])
+        return
+      }
+
+      setError('')
+      setMovies(data.results || [])
+
     } catch (error) {
       console.error('Error fetching movies:', error)
       setError('Error fetching movies')
+    } finally {
+      setIsLoading(false)
     }
   }
   useEffect(() => {
@@ -35,9 +50,9 @@ const App = () => {
       <div className='pattern'></div>
       <div className='wrapper'>
         <header>
-          <img 
-            src="./background.jpg" 
-            alt="hero banner" 
+          <img
+            src="./background.jpg"
+            alt="hero banner"
             className="inset-0 w-full h-full object-cover"
           />
           <h1>Find your favorite <span className='text-gradient'>movie</span></h1>
@@ -46,7 +61,13 @@ const App = () => {
         <h1 className='text-white'> {searchTitle} </h1>
         <section>
           <h2>All movies</h2>
-          {error && <p>{error}</p>}
+          {isLoading ? (<p className='text-white'>Loading...</p>) : error ? (<p className='text-white'>{error}</p>) :
+            (<ul>
+              {movies.map((movie: { id: number, original_title: string, overview: string }) => (
+                <li key={movie.id} className='text-white'>{movie.original_title}</li>
+              ))}
+            </ul>)}
+
         </section>
       </div>
     </div>
